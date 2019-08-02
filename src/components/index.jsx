a04e5d1bf15d37252c32f0ea4b8bb7291c5cbe08
Yisrael Grimes <yisraelgrimes@gmail.com>
2019-08-02 15:34:06 -0500

    updated react file names/paths

---
+++ src/components/index.jsx
@@ -0,0 +1,10 @@
+// Bulk import file.
+// To make components easier to import throughout the project.
+
+import Header from "./Header";
+import Image from "./Image";
+import Layout from "./Layout";
+import SEO from "./Seo";
+
+
+export { Header, Image, Layout, SEO };
